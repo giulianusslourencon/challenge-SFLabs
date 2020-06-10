33e35f4af21d49794d4fe5d549f4a9118f8c0eba
PSY <giulianolourencon@gmail.com>
2020-06-10 16:35:44 -0300

    Validaçao dos users e empresas

--- src/routes/empresaRoutes.js
+++ src/routes/empresaRoutes.js
@@ -1,11 +1,51 @@
 const routes = require('express').Router()
+const { validator } = require('cpf-cnpj-validator')
+const { celebrate, Segments } = require('celebrate')
+const Joi = require('@hapi/joi').extend(validator)
 
 const empresaController = require('../controllers/empresaController')
 
 routes.get('/', empresaController.index)
-routes.get('/:id', empresaController.show)
-routes.post('/', empresaController.create)
-routes.put('/:id', empresaController.update)
-routes.delete('/:id', empresaController.destroy)
+
+routes.get('/:id', celebrate({
+  [Segments.PARAMS]: Joi.object().keys({
+    id: Joi.string().required()
+  })
+}), empresaController.show)
+
+routes.post('/', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    _id: Joi.string().required(),
+    nome: Joi.string().required(),
+    cnpj: Joi.document().cnpj(),
+    cep: Joi.string().length(8),
+    endereco: Joi.string().required(),
+    numero: Joi.number().required(),
+    cidade: Joi.string().required(),
+    estado: Joi.string().required().length(2)
+  })
+}), empresaController.create)
+
+routes.put('/:id', celebrate({
+  [Segments.PARAMS]: Joi.object().keys({
+    id: Joi.string().required()
+  }),
+  [Segments.BODY]: Joi.object().keys({
+    _id: Joi.string().required(),
+    nome: Joi.string().required(),
+    cnpj: Joi.document().cnpj(),
+    cep: Joi.string().length(8),
+    endereco: Joi.string().required(),
+    numero: Joi.number().required(),
+    cidade: Joi.string().required(),
+    estado: Joi.string().required().length(2)
+  })
+}), empresaController.update)
+
+routes.delete('/:id', celebrate({
+  [Segments.PARAMS]: Joi.object().keys({
+    id: Joi.string().required()
+  })
+}), empresaController.destroy)
 
 module.exports = routes
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,44 @@
 const routes = require('express').Router()
+const { validator } = require('cpf-cnpj-validator')
+const { celebrate, Segments } = require('celebrate')
+const Joi = require('@hapi/joi').extend(validator)
 
 const userController = require('../controllers/userController')
 
 routes.get('/', userController.index)
-routes.get('/:id', userController.show)
-routes.post('/', userController.create)
-routes.put('/:id', userController.update)
-routes.delete('/:id', userController.destroy)
+
+routes.get('/:id', celebrate({
+  [Segments.PARAMS]: Joi.object().keys({
+    id: Joi.string().required()
+  })
+}), userController.show)
+
+routes.post('/', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    _id: Joi.string().required(),
+    nome: Joi.string().required(),
+    email: Joi.string().required().email(),
+    cpf: Joi.document().cpf(),
+    role: Joi.string().required()
+  })
+}), userController.create)
+
+routes.put('/:id', celebrate({
+  [Segments.PARAMS]: Joi.object().keys({
+    id: Joi.string().required()
+  }),
+  [Segments.BODY]: Joi.object().keys({
+    nome: Joi.string().required(),
+    email: Joi.string().required().email(),
+    cpf: Joi.document().cpf(),
+    role: Joi.string().required()
+  })
+}), userController.update)
+
+routes.delete('/:id', celebrate({
+  [Segments.PARAMS]: Joi.object().keys({
+    id: Joi.string().required()
+  })
+}), userController.destroy)
 
 module.exports = routes
