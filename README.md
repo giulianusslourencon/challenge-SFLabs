27a8cac7980390dbec4f943d45dcc81161ea380a
PSY <giulianolourencon@gmail.com>
2020-06-10 13:51:15 -0300

    Initial configuration

---
+++ README.md
@@ -0,0 +1,3 @@
+Selection challenge made for job evaluation by SF Labs.
+
+The challenge's statement is in the 'statement' folder, along with test data.
---
+++ statement/dados_1.js
@@ -0,0 +1,58 @@
+let empresas = [
+    {
+        "_id": "SFLABS",
+        "nome": "SF Labs",
+        "cnpj": "95.922.248/0001-16",
+        "cep": "13560-460",
+        "endereco": "Rua Alfredo Pereira Lopes",
+        "numero": "1717",
+        "cidade": "São Carlos",
+        "estado": "SP",
+        "users": [],
+        "empresa": [
+            {
+                "planta": "PLANTA_1"
+            },
+            {
+                "planta": "PLANTA_2"
+            }
+        ]
+    },
+    {
+        "_id": "PARQTEC",
+        "nome": "SF Labs",
+        "cnpj": "16.685.925/0001-53",
+        "cep": "13560-460",
+        "endereco": "Rua Alfredo Pereira Lopes",
+        "numero": "1717",
+        "cidade": "São Carlos",
+        "estado": "SP",
+        "users": [],
+        "empresa": [
+            {
+                "planta": "PLANTA_1"
+            },
+            {
+                "planta": "PLANTA_2"
+            }
+        ]
+    }
+]
+
+let users = [{
+    "_id": "jhonathan.viudes",
+    "nome": "Jhonathan Viudes",
+    "email": "jv@gmail.com",
+    "cpf": "346.206.720-32",
+    "role": 2,
+    "empresas": [],
+},
+{
+    "_id": "gustavo.tamanaka",
+    "nome": "Gustavo Tamanaka",
+    "email": "gustavo@gmail.com",
+    "cpf": "659.702.880-55",
+    "role": 4,
+    "empresas": [],
+}]
+
